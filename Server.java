79d2f6abef148fc044053c6338d8aadf2a723ff2
Maxim Brizhatyy <max.brijatyy@yandex.ru>
2023-09-27 21:16:54 +0700

    update class Server

--- Server.java
+++ Server.java
@@ -1,7 +1,30 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
-    public static void main(String[] args) {
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        int port = ConsoleHelper.readInt();
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            ConsoleHelper.writeMessage("Сервер запущен");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                Handler handler = new Handler(socket);
+                handler.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
