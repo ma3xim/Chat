65555305205a150cae546215575600a47ab1cb6d
Maxim Brizhatyy <max.brijatyy@yandex.ru>
2023-09-27 22:22:55 +0700

    step 7

--- Server.java
+++ Server.java
@@ -3,13 +3,27 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
     private static class Handler extends Thread {
         private Socket socket;
 
         public Handler(Socket socket) {
             this.socket = socket;
+        }
+    }
+
+    public static void sendBroadcastMessage(Message message){
+        for (Map.Entry<String,Connection> entry: connectionMap.entrySet()) {
+            Connection connection = entry.getValue();
+            try {
+                connection.send(message);
+            } catch (IOException e) {
+                ConsoleHelper.writeMessage("Ошибка отправки сообщения");
+            }
         }
     }
 
